7d3e3c0ff4fec60659c3ec91c0f7bfd7fc3bbf77
scokljat <selma.cokljat@hotmail.com>
2022-09-17 22:21:39 +0200

    Added modal to other pages

--- src/pages/myQuestions/MyQuestions.js
+++ src/pages/myQuestions/MyQuestions.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserQuestions } from "../../store/actions/Questions";
 import Details from "../../components/cards/details/Details";
+import Modal from "../../components/modal/Modal";
+import QuestionDetails from "../../components/modalContent/questionDetails/QuestionDetails";
 import { CardWrapper } from "../home/HomeStyle";
 import { Wrapper } from "../../globalStyles/GlobalStyles";
 
@@ -10,15 +12,30 @@ function MyQuestions() {
   const { user } = useSelector((state) => state.reducerUser);
   const { userQuestions } = useSelector((state) => state.reducerQuestions);
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [id, setId] = useState(0);
+
   useEffect(() => {
     dispatch(getUserQuestions(user?.id));
   }, [dispatch, user?.id]);
 
   return (
     <Wrapper>
+      {modalIsOpen && (
+        <Modal setIsOpen={setModalIsOpen}>
+          <QuestionDetails id={id} setModalIsOpen={setModalIsOpen} />
+        </Modal>
+      )}
       <CardWrapper height="85vh" width="100%">
         {userQuestions?.map((question) => {
-          return <Details question={question} key={question?.id} />;
+          return (
+            <Details
+              question={question}
+              key={question?.id}
+              setModalIsOpen={setModalIsOpen}
+              setId={setId}
+            />
+          );
         })}
       </CardWrapper>
     </Wrapper>
--- src/pages/questions/Questions.js
+++ src/pages/questions/Questions.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Details from "../../components/cards/details/Details";
 import FormCard from "../../components/cards/form/Form";
+import Modal from "../../components/modal/Modal";
+import QuestionDetails from "../../components/modalContent/questionDetails/QuestionDetails";
 import { getAllQuestions } from "../../store/actions/Questions";
 import { Wrapper } from "../../globalStyles/GlobalStyles";
 import { CardWrapper } from "../home/HomeStyle";
@@ -11,16 +13,29 @@ function Questions() {
   const { allQuestions } = useSelector((state) => state.reducerQuestions);
   const { isLoggedIn } = useSelector((state) => state.reducerUser);
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [id, setId] = useState(0);
+
   useEffect(() => {
     dispatch(getAllQuestions());
   }, [dispatch]);
 
   return (
     <Wrapper>
+      {modalIsOpen && (
+        <Modal setIsOpen={setModalIsOpen}>
+          <QuestionDetails id={id} setModalIsOpen={setModalIsOpen} />
+        </Modal>
+      )}
       {isLoggedIn && <FormCard placeholder="What's on your mind ..." />}
       <CardWrapper height={isLoggedIn ? "65vh" : "85vh"} width="100%">
         {allQuestions.map((question) => (
-          <Details question={question} key={question.id} />
+          <Details
+            question={question}
+            key={question.id}
+            setModalIsOpen={setModalIsOpen}
+            setId={setId}
+          />
         ))}
       </CardWrapper>
     </Wrapper>

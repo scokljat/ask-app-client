9c0520d40ede98b9d9452caee9eec53b98d7ff86
scokljat <selma.cokljat@hotmail.com>
2022-09-18 21:05:27 +0200

    Added style for error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
+import { ReactComponent as Frown } from "../assets/images/frown.svg";
+import { Wrapper } from "../globalStyles/GlobalStyles";
+import { Description } from "../components/cards/details/DetailsStyle";
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <Wrapper style={{ marginTop: "9.7rem" }}>
+      <Frown />
+      <Description>404</Description>
+      <Description>Page not found</Description>
+    </Wrapper>
+  );
 }
 
 export default Error;

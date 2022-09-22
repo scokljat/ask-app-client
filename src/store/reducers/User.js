21dc70095c8319dca31c5ea820c06394852af799
scokljat <selma.cokljat@hotmail.com>
2022-09-22 12:08:10 +0200

    Added new methods for user

--- src/store/reducers/User.js
+++ src/store/reducers/User.js
@@ -1,8 +1,16 @@
-import { REGISTER, LOGIN, FETCH_USER } from "../constants/ActionTypes";
+import {
+  REGISTER,
+  LOGIN,
+  FETCH_USER,
+  FETCH_POPULAR_USERS,
+  UPDATE_USER,
+  LOGOUT,
+} from "../constants/ActionTypes";
 
 const initialState = {
   user: {},
   isLoggedIn: localStorage.getItem("token") ? true : false,
+  popularUsers: [],
 };
 
 export const reducerUser = (state = initialState, { type, payload }) => {
@@ -11,10 +19,21 @@ export const reducerUser = (state = initialState, { type, payload }) => {
       localStorage.setItem("token", payload.accessToken);
       return { ...state, user: payload.user, isLoggedIn: true };
     case LOGIN:
+      console.log(payload);
       localStorage.setItem("token", payload.accessToken);
       return { ...state, user: payload.user, isLoggedIn: true };
+    case LOGOUT:
+      localStorage.removeItem("token");
+      return { ...state, user: {}, isLoggedIn: false };
     case FETCH_USER:
       return { ...state, user: payload };
+    case FETCH_POPULAR_USERS:
+      return { ...state, popularUsers: payload.reverse() };
+    case UPDATE_USER:
+      return {
+        ...state,
+        user: payload,
+      };
     default:
       return state;
   }

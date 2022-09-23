23fa83fa89c2dbbdf72bb342aa83fa335d826374
scokljat <selma.cokljat@hotmail.com>
2022-09-23 22:11:36 +0200

    Updated service

--- src/api/services/Questions.js
+++ src/api/services/Questions.js
@@ -9,62 +9,52 @@ const QuestionsService = {
   getAllQuestions: () => {
     const url =
       "/questions?_expand=user&_embed=likes&_embed=dislikes&_sort=dateOfPublished";
-
     return axios.get(url);
   },
 
   getUserQuestions: (id) => {
     const url = `/questions?userId=${id}&_expand=user&_embed=likes&_embed=dislikes&_sort=dateOfPublished`;
-
     return axios.get(url);
   },
 
   getQuestionById: (id) => {
     const url = `/questions/${id}/?_expand=user&_embed=answers&_embed=likes&_embed=dislikes`;
-
     return axios.get(url);
   },
 
   getHotQuestions: () => {
     const url =
       "/questions?_expand=user&_embed=likes&_embed=dislikes&_sort=numberOfLikes";
-
     return axios.get(url);
   },
 
   createQuestion: (question) => {
     const url = "/questions";
-
     return axios.post(url, question);
   },
 
   likeQuestion: (likedQuestion) => {
     const url = "/likes";
-
     return axios.post(url, likedQuestion);
   },
 
   dislikeQuestion: (dislikedQuestion) => {
     const url = "/dislikes";
-
     return axios.post(url, dislikedQuestion);
   },
 
   deleteQuestion: (id) => {
     const url = `/questions/${id}`;
-
     return axios.delete(url);
   },
 
   updateQuestion: (updatedQuestion) => {
     const url = `/questions/${updatedQuestion.id}`;
-
     return axios.put(url, updatedQuestion);
   },
 
   increaseQuestionLikes: (likedQuestion) => {
     const url = `/questions/${likedQuestion.id}`;
-
     return axios.put(url, likedQuestion);
   },
 };
